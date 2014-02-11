5032fc5db404573bb58f3ecc499e13beac72d6dd
Austin B <agblackmon@gmail.com>
2014-02-10 19:52:08 -0500

    Initial check-in for ruby rails tutorial

---
+++ app/views/statuses/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @status, :id, :name, :concent, :created_at, :updated_at
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Treebook::Application.config.secret_key_base = '7727aff7e5d783b82389a573de80bcbd8da418aab785216502060302ed45c6688407e4dda34ab5ad0292f0a0e247ce656b1da10c1655bc9e33c5638176a4d9ee'
